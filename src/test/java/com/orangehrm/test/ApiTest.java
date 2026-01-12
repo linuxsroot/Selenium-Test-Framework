ab65ee9d4afb4d1469b83d19aa3f301dd01001b7
linuxsroot <rootgly22@gmail.com>
2026-01-12 20:32:40 +0530

    removed comments

--- src/test/java/com/orangehrm/test/ApiTest.java
+++ src/test/java/com/orangehrm/test/ApiTest.java
@@ -14,8 +14,6 @@ public class ApiTest {
 
         SoftAssert softAssert = new SoftAssert();
 
-        //Test Checking
-
         //Step1: Define API endpoint
         String endPoint = "https://jsonplaceholder.typicode.com/users/1";
         ExtentManager.logStep("API Endpoint: "+endPoint);
